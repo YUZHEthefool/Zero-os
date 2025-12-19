99fd59d203e414fd33cf65f5965094e1a68740cd
YUZHEthefool <2804776511@qq.com>
2025-12-19 01:34:30 +0000

    chore: remove build artifacts from tracking

--- userspace-target/release/build/compiler_builtins-9aa5e610d65241dd/build_script_build-9aa5e610d65241dd.d
+++
@@ -1,6 +0,0 @@
-/home/dev/workspace/project/rsproject/Zero-os-main/userspace/../userspace-target/release/build/compiler_builtins-9aa5e610d65241dd/build_script_build-9aa5e610d65241dd.d: /home/dev/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs /home/dev/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs
-
-/home/dev/workspace/project/rsproject/Zero-os-main/userspace/../userspace-target/release/build/compiler_builtins-9aa5e610d65241dd/build_script_build-9aa5e610d65241dd: /home/dev/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs /home/dev/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs
-
-/home/dev/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/build.rs:
-/home/dev/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/compiler-builtins/compiler-builtins/configure.rs:
--- userspace-target/x86_64-unknown-none/release/deps/shell-abab2f79dced99a5.d
+++
@@ -1,5 +0,0 @@
-/home/dev/workspace/project/rsproject/Zero-os-main/userspace/../userspace-target/x86_64-unknown-none/release/deps/shell-abab2f79dced99a5.d: src/shell.rs
-
-/home/dev/workspace/project/rsproject/Zero-os-main/userspace/../userspace-target/x86_64-unknown-none/release/deps/shell-abab2f79dced99a5: src/shell.rs
-
-src/shell.rs:
